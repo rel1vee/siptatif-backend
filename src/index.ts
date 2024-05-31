fa3adef6fe93cf743153a7d905acbe908e430a03
unknown <muhzakierbaisyas@gmail.com>
2024-05-31 19:33:49 +0700

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response, NextFunction} from "express";
+
+const app: Application = express()
+const port: Number = 3000
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({
+        status: "200",
+        message: "Hello, World!"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
