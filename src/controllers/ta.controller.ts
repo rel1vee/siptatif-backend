d4599e7c049084e091971e9b610732f199a1fc76
unknown <muhzakierbaisyas@gmail.com>
2024-06-02 17:49:54 +0700

    feat: upload file

--- src/controllers/ta.controller.ts
+++ src/controllers/ta.controller.ts
@@ -47,6 +47,11 @@ export const createTA = async (req: Request, res: Response) => {
     });
   }
 
+  // Pengecekan keberadaan file
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).send({ status: false, statusCode: 400, message: 'No files were uploaded' });
+  }
+
   try {
     const files = req.files as Express.Multer.File[]; // Mendapatkan array of files dari request
     const fileBuffers = files.map((file) => file.buffer); // Mengubah file menjadi buffer
